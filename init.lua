305baef090670f5040c62e6e2252cd663233dc92
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-04-25 20:25:20 +0700

    feat: colorscheme

--- init.lua
+++ init.lua
@@ -19,5 +19,8 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- [[ Colorscheme ]]
+require 'colorscheme'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = 'tokyonight-night'
+
+local status_ok, _ = pcall(function()
+    vim.cmd('colorscheme ' .. colorscheme)
+end)
+if not status_ok then
+    vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+    return
+end
