1f3744e2a63b85bce87d6bd692addd57fd34d767
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-04-25 08:22:00 +0700

    feat(plugins->gitsigns): remove a bunch of unnecessary things :D

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -48,6 +48,8 @@ require('lazy').setup({
 
     require 'plugins/treesitter',
 
+    require 'plugins/neo-tree'
+
     -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
     -- place them in the correct locations.
@@ -57,11 +59,11 @@ require('lazy').setup({
     --  Here are some example plugins that I've included in the Kickstart repository.
     --  Uncomment any of the lines below to enable them (you will need to restart nvim).
     --
-    -- require 'kickstart.plugins.debug',
-    -- require 'kickstart.plugins.indent_line',
-    -- require 'kickstart.plugins.lint',
-    -- require 'kickstart.plugins.autopairs',
-    -- require 'kickstart.plugins.neo-tree',
+    -- require 'plugins.debug',
+    -- require 'plugins.indent_line',
+    -- require 'plugins.lint',
+    -- require 'plugins.autopairs',
+    -- require 'plugins.neo-tree',
 
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
