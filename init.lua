f17301778470d37b4c8c3b8d952736ee2e2bff21
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-04-26 08:56:44 +0700

    feat(dashboard)

--- init.lua
+++ init.lua
@@ -4,6 +4,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- disable netrw at startup the `:Lexplore` command
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
