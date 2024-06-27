3ed1070d69aa7300e640c03a4f1403b0bc5fa96b
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-06-27 10:03:05 +0700

    feat(harpoon): update

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,19 +8,34 @@ return {
             require('harpoon').setup {}
 
             local function toggle_telescope_with_harpoon(harpoon_files)
-                local file_paths = {}
-                for _, item in ipairs(harpoon_files.items) do
-                    table.insert(file_paths, item.value)
+                local finder = function()
+                    local paths = {}
+                    for _, item in ipairs(harpoon_files.items) do
+                        table.insert(paths, item.value)
+                    end
+
+                    return require('telescope.finders').new_table {
+                        results = paths,
+                    }
                 end
 
                 require('telescope.pickers')
                     .new({}, {
                         prompt_title = 'Harpoon',
-                        finder = require('telescope.finders').new_table {
-                            results = file_paths,
-                        },
+                        finder = finder(),
                         previewer = require('telescope.config').values.file_previewer {},
                         sorter = require('telescope.config').values.generic_sorter {},
+                        attach_mappings = function(prompt_bufnr, map)
+                            map('i', '<C-d>', function()
+                                local state = require 'telescope.actions.state'
+                                local selected_entry = state.get_selected_entry()
+                                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                                table.remove(harpoon_files.items, selected_entry.index)
+                                current_picker:refresh(finder())
+                            end)
+                            return true
+                        end,
                     })
                     :find()
             end
@@ -40,20 +55,20 @@ return {
         keys = function()
             local keys = {
                 {
-                    '<leader>hx',
+                    '<leader>ha',
                     function()
                         require('harpoon'):list():add()
                     end,
-                    desc = 'Mark file with Harpoon',
+                    desc = 'Add mark file using Harpoon',
                 },
-                --[[ {
-                    '<leader>h',
+                {
+                    '<C-q>',
                     function()
                         local harpoon = require 'harpoon'
                         harpoon.ui:toggle_quick_menu(harpoon:list())
                     end,
                     desc = 'Harpoon Quick Menu',
-                }, ]]
+                },
             }
 
             for i = 1, 5 do
