641675bcd57bed7f080580c32d4581f36ada4bc9
Wahyu Syahputra <syahputrawahyu61@gmail.com>
2024-08-07 13:32:24 +0700

    feat: update comfortable-motion conf

--- lua/plugins/comfortable-motion.lua
+++ lua/plugins/comfortable-motion.lua
@@ -1,3 +1,8 @@
 return {
     'yuttie/comfortable-motion.vim',
+    config = function()
+        -- VimTeX configuration goes here, e.g.
+        vim.api.nvim_set_keymap('n', '<C-d>', [[:call comfortable_motion#flick(75)<CR>]], { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<C-u>', [[:call comfortable_motion#flick(-75)<CR>]], { noremap = true, silent = true })
+    end,
 }
